38a40cf4deefbc34de0a839d21170c2148ff869f
Oleksandr <alexitmcaf@gmail.com>
2023-10-07 21:21:05 -0300

    test commit

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -9,10 +9,6 @@
 
     'author': "My Company",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
